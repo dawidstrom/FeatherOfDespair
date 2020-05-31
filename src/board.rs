029f1e716f269341b495e1bf3f1cbfcd2917bccf
dawidstrom <dawid.strom@gmail.com>
2020-05-31 21:50:03 +0200

    Add functions to load/save tilemap from/to file.

--- src/board.rs
+++ src/board.rs
@@ -1,6 +1,12 @@
 use crate::entity::*;
 use crate::utils::*;
 
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+use std::{
+    fs::File,
+    io::{self, Read, Write},
+};
+
 pub trait Movable {
     fn move_dir(
         &mut self,
@@ -17,10 +23,74 @@ pub enum Direction {
     Left,
 }
 
+pub enum Tile {
+    WALL = 1,
+}
+
 pub struct Board {
-    pub size:       Size,
-    pub scale:      i32,
-    pub blocking_map: Vec<Entity>,
+    pub size:           Size,
+    pub scale:          i32,
+    pub blocking_map:   Vec<Entity>,
+}
+
+impl Board {
+    pub fn readWall(reader: &mut impl Read) -> Option<Entity> {
+        if let (
+            Ok(x), 
+            Ok(y), 
+            Ok(blocking)
+        ) = (
+            reader.read_i32::<LittleEndian>(),
+            reader.read_i32::<LittleEndian>(),
+            reader.read_u8()
+        )
+        {
+            return Some(Entity{
+                pos: Position{ x,y },
+                blocking: blocking != 0,
+            })
+        }
+        None
+    }
+
+    pub fn load(reader: &mut impl Read) -> io::Result<Self> {
+        // Board size.
+        let width = reader.read_i32::<LittleEndian>().unwrap();
+        let height = reader.read_i32::<LittleEndian>().unwrap();
+
+        // Board scale factor.
+        let scale = reader.read_i32::<LittleEndian>().unwrap();
+
+        // Entities.
+        let mut walls = Vec::<Entity>::new();
+        while let Some(wall) = Board::readWall(reader) {
+            walls.push(wall);
+        }
+
+        Ok(Board {
+            size: Size{ width, height },
+            scale,
+            blocking_map: walls,
+        })
+    }
+
+    pub fn write(self, mut writer: impl byteorder::WriteBytesExt) {
+        // Board size.
+        writer.write_i32::<LittleEndian>(self.size.width);
+        writer.write_i32::<LittleEndian>(self.size.height);
+
+        // Board scale factor.
+        writer.write_i32::<LittleEndian>(self.scale);
+
+        // Entities.
+        for wall in self.blocking_map {
+            // Position.
+            writer.write_i32::<LittleEndian>(wall.pos.x);
+            writer.write_i32::<LittleEndian>(wall.pos.y);
+            // Is blocking.
+            writer.write_u8(wall.blocking as u8);
+        }
+    }
 }
 
 impl Movable for Entity {
